68e9223f76b9a87ea22a3e2b4de9de3aad9beccd
Muktar <mayunited12@yahoo.com>
2022-06-19 15:32:33 +0100

    import scss variable file globally

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+  images: {
+    disableStaticImages: true
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "variables.module.scss";`
+  }
 }
