c24ae16ce69d0cb278919da1e1408b2b5475d861
Muktar <mayunited12@yahoo.com>
2022-06-20 15:56:14 +0100

    sidenav progress

--- pages/components/sideMenu/SideNav.js
+++ pages/components/sideMenu/SideNav.js
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import { FaBeer } from 'react-icons/fa';
+// import { useState } from "react";
+// import { FaBeer } from 'react-icons/fa';
 
 export default function Sidenav () {
-    const [nav, setNav] = useState([
-        {
-            icon: "ball",
-            title: "menu 1"
-        }
-    ])
+    // const [nav, setNav] = useState([
+    //     {
+    //         icon: "ball",
+    //         title: "menu 1"
+    //     }
+    // ])
     return (
     <div>
-      <p>You clicked {count} times</p>
+      {/* <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
-      </button>
+      </button> */}
     </div>
   );
 }
