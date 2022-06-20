7d9c09a7d3d29255a262c7afed98b2a84ee31b46
Muktar <mayunited12@yahoo.com>
2022-06-20 15:57:43 +0100

    implement app footer

---
+++ pages/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+      <footer className="footer">
+        <a
+          href="https://twitter.com/whoismuktar"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Designed with ❤️ by whoIsMuktar
+        </a>
+      </footer>
+  )
+}
