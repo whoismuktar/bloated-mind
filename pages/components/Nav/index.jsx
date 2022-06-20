91429f83426afd26881f476f2702411d4b56ccfe
Muktar <mayunited12@yahoo.com>
2022-06-20 15:56:06 +0100

    connect nav rout links

--- pages/components/Nav/index.jsx
+++ pages/components/Nav/index.jsx
@@ -2,14 +2,15 @@
 import { useRouter } from "next/router"
 import styles from "./nav.module.scss"
 import Logo from "./Logo"
+import Link from "next/link";
 
 export default function Nav() {
     const router = useRouter();
 
     return (
         <div className={`${styles["nav-wrapper"]}`}>
-            <nav className={`${styles["nav"]} container px-4 mx-auto`}>
-                <div className="py-6 sm:px-6 lg:px-8">
+            <nav className={`${styles["nav"]} container mx-auto`}>
+                <div className="py-6">
                     <div className="flex items-center justify-between h-16">
                         <div className="flex items-center">
                             <div className={`${styles["_logo"]} flex-shrink-0`}>
@@ -17,11 +18,17 @@ export default function Nav() {
                             </div>
                             <div className="hidden md:block">
                                 <div className="ml-10 flex items-baseline space-x-4">
-                                    <a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+                                    <Link href="/">
+                                        <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+                                    </Link>
 
-                                    <a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Get Started</a>
+                                    <Link href="/connect">
+                                        <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Get Started</a>
+                                    </Link>
 
-                                    <a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Terms</a>
+                                    <Link href="/terms">
+                                        <a className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Terms</a>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
