9c1e195eea828c04c66c12f98f9aeed899755b42
Muktar <mayunited12@yahoo.com>
2022-06-20 15:57:59 +0100

    implement chat box progress

---
+++ pages/components/ChatBox/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./chat-box.module.scss"
+
+export default function ChatBox() {
+  return (
+    <div className={styles.chatbox}>
+        <div className="message-wrapper">
+            
+        </div>
+    </div>
+  )
+}
