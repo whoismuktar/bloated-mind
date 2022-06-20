438d78c958ca6073c05aebe22875a7ff9b82981a
Muktar <mayunited12@yahoo.com>
2022-06-20 22:12:49 +0100

    SSR supported Emoji picker for emoji-mart

---
+++ pages/components/EmojiPicker/index.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from 'react'
+import data from '@emoji-mart/data'
+
+function EmojiPicker(props = {}) {
+  const ref = useRef()
+  const addEmoji = props.addEmoji
+
+  useEffect(() => {
+    import('emoji-mart').then((EmojiMart) => {
+      new EmojiMart.Picker({ ...props, data, ref })
+    })
+  }, [])
+
+  return<div className="emoji-pack" onSelect={addEmoji} title="Holla" ref={ref}></div>
+}
+
+export default EmojiPicker
