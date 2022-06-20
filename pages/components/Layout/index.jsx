55dab3f7c3c421eb6437f772282d6814e336dabc
Muktar <mayunited12@yahoo.com>
2022-06-20 15:58:12 +0100

    implement app layout

---
+++ pages/components/Layout/index.jsx
@@ -0,0 +1,15 @@
+import Footer from "../Footer"
+import Nav from "../Nav"
+
+export default function index({ pageConfig, children }) {
+    console.log({ pageConfig, children });
+    const { noFooter, noNav, noTopSpace } = pageConfig
+
+    return (
+        <div className="app-wrapper">
+            {!noNav && <Nav></Nav>}
+            <main className={`${noNav ? "no-nav" : ""}${noFooter ? "no-footer" : ""}${noTopSpace? "no-top-space" : ""} main-body container mx-auto`}>{children}</main>
+            {!noFooter && <Footer></Footer>}
+        </div >
+    )
+}
