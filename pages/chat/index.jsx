ca723e8543afa2006798c2d5a78b09de763c2339
Muktar <mayunited12@yahoo.com>
2022-07-03 18:00:38 +0100

    implement isMobile conditional & mobile view

--- pages/chat/index.jsx
+++ pages/chat/index.jsx
@@ -1,19 +1,26 @@
+import { useState, useEffect } from "react"
 import UserChatCard from "../components/UserChatCard"
 import ChatBox from "../components/ChatBox"
-import { useEffect } from "react"
 import styles from "./chat.module.scss"
+import { isMobile } from "../../app.config"
 
 export default function Chat() {
-  // useEffect(()=> {
-  // })
+  const [onMobile, setOnMobile] = useState("")
+
+    useEffect(()=> {
+        if(isMobile) {
+            setOnMobile("on-Mobile")
+        }
+    }, [])
+
   return (
-    <div className={styles["chat-pg"]}>
+    <div className={`${styles["chat-pg"]} ${styles[onMobile]}`}>
       <div className={styles["chat-wrapper"]}>
-        <div className={styles["usercard-wrapper"]}>
+        <div className={`${styles["usercard-wrapper"]} hidden sm:block`}>
           <UserChatCard />
         </div>
 
-        <div className={styles["chatbox-wrapper"]}>
+        <div className={`${styles["chatbox-wrapper"]}`}>
           <ChatBox />
         </div>
       </div>
--- pages/components/ChatBox/index.jsx
+++ pages/components/ChatBox/index.jsx
@@ -1,9 +1,10 @@
-import { BsEmojiSmile } from "react-icons/bs"
-import { FaTelegramPlane } from "react-icons/fa"
 import UserAvi from "../userAvi"
 import EmojiPicker from "../EmojiPicker"
-import { useState } from "react"
 import styles from "./chat-box.module.scss"
+import { useState, useEffect } from "react"
+import { isMobile } from "../../../app.config"
+import { BsEmojiSmile } from "react-icons/bs"
+import { FaTelegramPlane } from "react-icons/fa"
 
 export default function ChatBox() {
     const dummyConversations = [
@@ -32,6 +33,7 @@ export default function ChatBox() {
     const [currentUser, setCurrentUser] = useState(1)
     const [showEmoji, setShowEmoji] = useState(false)
     const [conversation, setConversation] = useState(dummyConversations)
+    const [onMobile, setOnMobile] = useState("")
 
     const toggleEmoji = () => {
         setShowEmoji(!showEmoji)
@@ -58,9 +60,33 @@ export default function ChatBox() {
         setMessage("")
     }
 
+    useEffect(()=> {
+        if(isMobile) {
+            setOnMobile("on-Mobile")
+        }
+    }, [])
+
+    console.log({onMobile})
+
     return (
-        <div className={styles.chatbox}>
-            <div className={styles.conversations}>
+        <div className={`${styles.chatbox} ${styles[onMobile]}`}>
+            {
+                onMobile && <div className={styles["conv-user-card"]}>
+                    <div className={`${styles["user-avatar"]}`}>
+                        <UserAvi aviWidth={50} aviHeight={50} />
+                    </div>
+                        <div className={`${styles["user-meta"]}`}>
+                            <h1 className={`${styles["user-name"]}`}>Username</h1>
+
+                            <div className={`${styles["user-status"]}`}>
+                                <span className={`${styles["status-online"]}`}>Online</span>
+                                <span className={`${styles["status-away"]}`}>Away</span>
+                                <span className={`${styles["status-offline"]}`}>Offline</span>
+                            </div>
+                        </div>
+                </div>
+            }
+            <div className={`${styles.conversations}`}>
                 {
                     conversation.map((conv, i) => {
                         return (
@@ -68,7 +94,7 @@ export default function ChatBox() {
                                 <div className={styles["message-wrapper"]}>
                                     {
                                         conv.author != currentUser && <div className={styles["user-avatar"]}>
-                                            <UserAvi aviWidth="50" aviHeight="50" />
+                                            <UserAvi aviWidth="30" aviHeight="30" />
                                         </div>
                                     }
 
@@ -96,10 +122,12 @@ export default function ChatBox() {
             </div>
 
             <div className={styles["message-input"]}>
-                <div className={styles["emoji-wrapper"]} onClick={toggleEmoji}>
-                    <BsEmojiSmile />
-                    {showEmoji && <EmojiPicker onEmojiSelect={addEmoji} preview={false} />}
-                </div>
+                { !isMobile &&
+                    <div className={styles["emoji-wrapper"]} onClick={toggleEmoji}>
+                        <BsEmojiSmile />
+                        {showEmoji && <EmojiPicker onEmojiSelect={addEmoji} preview={false} />}
+                    </div>
+                }
                 <textarea
                     type="text"
                     name="messageInput"
